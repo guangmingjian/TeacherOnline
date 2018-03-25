a1ddf66092cb935765b111aeb8f71d1354b894e7
guangmingjian <1106697389@qq.com>
2018-03-25 18:19:43 +0800

    ajax用户名存在提醒

--- src/main/webapp/static/js/log.js
+++ src/main/webapp/static/js/log.js
@@ -3,18 +3,19 @@ $(function() {
 		console.log("验证是否用户名存在");
         var htmlco = $("#inputEmail").val();
         //alert(htmlco);
+        // console.log(htmlco);
         $.ajax(
-            {data:{'email':htmlco},
-                dataType:'email',
+            {data: {'email':htmlco},
+                dataType:"json",
                 success: function(data){
                 console.log(data);
-                    alert(data);
+                    alert("提交成功");
                     //  alert(searchUrl);
                     //window.location.href = searchUrl;
+                    $("#input-tip").html("请输入正确的用户名");
                 },
                 type:'post',
-                url:'/UserExist',
-                error:alert("错误")
+                url:'/UserExist'
             })
 	});
 	// $(function () { $('#collapseTwo').collapse('show')});
